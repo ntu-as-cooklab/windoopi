19bb198732710b1f1d756dab3f0269ea436c225d
En Shih <seanstone5923@gmail.com>
2016-06-22 23:46:14 +0800

    PortAudio examples

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,22 @@ int main()
     err = Pa_Initialize();
     if( err != paNoError ) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 
+    /*#define SAMPLE_RATE (44100)
+    static paTestData data;
+
+    PaStream *stream;
+    // Open an audio I/O stream.
+    err = Pa_OpenDefaultStream( &stream,
+                                0,          // no input channels
+                                2,          // stereo output
+                                paFloat32,  // 32 bit floating point output
+                                SAMPLE_RATE,
+                                256,        // frames per buffer, i.e. the number of sample frames that PortAudio will request from the callback.
+                                            // Many apps may want to use paFramesPerBufferUnspecified, which tells PortAudio to pick the best, possibly changing, buffer size.
+                                patestCallback, // this is your callback function
+                                &data ); // This is a pointer that will be passed to your callback
+    if( err != paNoError ) printf("PortAudio error: %s\n", Pa_GetErrorText(err));*/
+
     err = Pa_Terminate();
     if( err != paNoError ) printf("PortAudio error: %s\n", Pa_GetErrorText(err));
 
